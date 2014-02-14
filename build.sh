079b67f392da0ed19f449831f8777b0bb42cead4
Kerri Shotts <kerrishotts@gmail.com>
2014-02-14 15:24:39 -0600

    Start move to SASS

--- build.sh
+++ build.sh
@@ -6,5 +6,18 @@
 #
 # requires node to be installed
 #
+#
+# Copy assets
+echo "Copying assets..."
+cp -r ./lib/yasmf-assets ./dist
+#
+# Build RequireJS
+echo "RequireJS Build..."
 node ./vendor/requirejs/node_modules/requirejs/bin/r.js -o ./build.js
+#
+# compile SASS (requires sass from sass-lang.com)
+echo "SASS Build..."
+sass --trace ./lib/yasmf.scss ./dist/yasmf.css
+echo "Done."
+
 
