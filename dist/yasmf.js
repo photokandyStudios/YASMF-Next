ca9002a4199d6947d2aae04b3c9de686c077a907
Kerri Shotts <kerrishotts@gmail.com>
2014-10-05 23:55:28 -0500

    new diet

--- dist/yasmf.js
+++ dist/yasmf.js
@@ -2147,8 +2147,322 @@ define( 'yasmf/util/core',[ "globalize", "cultures/globalize.culture.en-US" ], f
       proto[ i[ 0 ] ] = i[ 1 ];
     }
   } );
+  /**
+   * Returns a value for the specified keypath. If any intervening
+   * values evaluate to undefined or null, the entire result is
+   * undefined or null, respectively.
+   *
+   * If you need a default value to be returned in such an instance,
+   * specify it after the keypath.
+   *
+   * Note: if `o` is not an object, it is assumed that the function
+   * has been bound to `this`. As such, all arguments are shifted by
+   * one position to the right.
+   *
+   * Key paths are of the form:
+   *
+   *    object.field.field.field[index]
+   *
+   * @param {object} o        the object to search
+   * @param {string} k        the keypath
+   * @param {*} d             (optional) the default value to return
+   *                          should the keypath evaluate to null or
+   *                          undefined.
+   * @return {*}              the value at the keypath
+   *
+   * License MIT: Copyright 2014 Kerri Shotts
+   */
+  function valueForKeyPath( o, k, d ) {
+    if ( o === undefined || o === null ) {
+      return ( d !== undefined ) ? d : o;
+    }
+    if ( !( o instanceof Object ) ) {
+      d = k;
+      k = o;
+      o = this;
+    }
+    var v = o;
+    // There's a million ways that this regex can go wrong
+    // with respect to JavaScript identifiers. Splits will
+    // technically work with just about every non-A-Za-z\$-
+    // value, so your keypath could be "field/field/field"
+    // and it would work like "field.field.field".
+    v = k.match( /([\w\$\\\-]+)/g ).reduce( function ( v, keyPart ) {
+      if ( v === undefined || v === null ) {
+        return v;
+      }
+      try {
+        return v[ keyPart ];
+      } catch ( err ) {
+        return undefined;
+      }
+    }, v );
+    return ( ( v === undefined || v === null ) && ( d !== undefined ) ) ? d : v;
+  }
+  /**
+   * Interpolates values from the context into the string. Placeholders are of the
+   * form {...}. If values within {...} do not exist within context, they are
+   * replaced with undefined.
+   * @param  {string} str     string to interpolate
+   * @param  {*} context      context to use for interpolation
+   * @return {string}}        interpolated string
+   */
+  function interpolate( str, context ) {
+    var newStr = str;
+    if ( typeof context === "undefined" ) {
+      return newStr;
+    }
+    str.match( /\{([^\}]+)\}/g ).forEach( function ( match ) {
+      var prop = match.substr( 1, match.length - 2 ).trim();
+      newStr = newStr.replace( match, valueForKeyPath( context, prop ) );
+    } );
+    return newStr;
+  }
+  /**
+   * Merges the supplied objects together and returns a copy containin the merged objects. The original
+   * objects are untouched, and a new object is returned containing a relatively deep copy of each object.
+   *
+   * Important Notes:
+   *   - Items that exist in any object but not in any other will be added to the target
+   *   - Should more than one item exist in the set of objects with the same key, the following rules occur:
+   *     - If both types are arrays, the result is a.concat(b)
+   *     - If both types are objects, the result is merge(a,b)
+   *     - Otherwise the result is b (b overwrites a)
+   *   - Should more than one item exist in the set of objects with the same key, but differ in type, the
+   *     second value overwrites the first.
+   *   - This is not a true deep copy! Should any property be a reference to another object or array, the
+   *     copied result may also be a reference (unless both the target and the source share the same item
+   *     with the same type). In other words: DON'T USE THIS AS A DEEP COPY METHOD
+   *
+   * It's really meant to make this kind of work easy:
+   *
+   * var x = { a: 1, b: "hi", c: [1,2] },
+   *     y = { a: 3, c: [3, 4], d: 0 },
+   *     z = merge (x,y);
+   *
+   * z is now { a: 3, b: "hi", c: [1,2,3,4], d:0 }.
+   *
+   * License MIT. Copyright Kerri Shotts 2014
+   */
+  function merge() {
+    var t = {},
+      args = Array.prototype.slice.call( arguments, 0 );
+    args.forEach( function ( s ) {
+      Object.keys( s ).forEach( function ( prop ) {
+        var e = s[ prop ];
+        if ( e instanceof Array ) {
+          if ( t[ prop ] instanceof Array ) {
+            t[ prop ] = t[ prop ].concat( e );
+          } else if ( !( t[ prop ] instanceof Object ) || !( t[ prop ] instanceof Array ) ) {
+            t[ prop ] = e;
+          }
+        } else if ( e instanceof Object && t[ prop ] instanceof Object ) {
+          t[ prop ] = merge( t[ prop ], e );
+        } else {
+          t[ prop ] = e;
+        }
+      } );
+    } );
+    return t;
+  }
+  /**
+   * Validates a source against the specified rules. `source` can look like this:
+   *
+   *     { aString: "hi", aNumber: { hi: 294.12 }, anInteger: 1944.32 }
+   *
+   * `rules` can look like this:
+   *
+   *     {
+   *       "a-string": {
+   *         title: "A String",     -- optional; if not supplied, key is used
+   *         key: "aString",        -- optional: if not supplied the name of this rule is used as the key
+   *         required: true,        -- optional: if not supplied, value is not required
+   *         type: "string",        -- string, number, integer, array, date, boolean, object, *(any)
+   *         minLength: 1,          -- optional: minimum length (string, array)
+   *         maxLength: 255         -- optional: maximum length (string, array)
+   *       },
+   *       "a-number": {
+   *         title: "A Number",
+   *         key: "aNumber.hi",     -- keys can have . and [] to reference properties within objects
+   *         required: false,
+   *         type: "number",
+   *         min: 0,                -- if specified, number/integer can't be smaller than this number
+   *         max: 100               -- if specified, number/integer can't be larger than this number
+   *       },
+   *       "an-integer": {
+   *         title: "An Integer",
+   *         key: "anInteger",
+   *         required: true,
+   *         type: "integer",
+   *         enum: [1, 2, 4, 8]     -- if specified, the value must be a part of the array
+   *                                -- may also be specified as an array of objects with title/value properties
+   *       }
+   *     }
+   *
+   * @param {*} source       source to validate
+   * @param {*} rules        validation rules
+   * @returns {*}            an object with two fields: `validates: true|false` and `message: validation message`
+   *
+   * LICENSE: MIT
+   * Copyright Kerri Shotts, 2014
+   */
+  function validate( source, rules ) {
+    var r = {
+      validates: true,
+      message: ""
+    };
+    if ( !( rules instanceof Object ) ) {
+      return r;
+    }
+    // go over each rule in `rules`
+    Object.keys( rules ).forEach( function ( prop ) {
+      if ( r.validates ) {
+        // get the rule
+        var rule = rules[ prop ],
+          v = source,
+          // and get the value in source
+          k = ( rule.key !== undefined ) ? rule.key : prop,
+          title = ( rule.title !== undefined ) ? rule.title : prop;
+        k = k.replace( "[", "." ).replace( "]", "" ).replace( "\"", "" );
+        k.split( "." ).forEach( function ( keyPart ) {
+          try {
+            v = v[ keyPart ];
+          } catch ( err ) {
+            v = undefined;
+          }
+        } );
+        // is it required?
+        if ( ( ( rule.required !== undefined ) ? rule.required : false ) && v === undefined ) {
+          r.validates = false;
+          r.message = "Missing required value " + title;
+          return;
+        }
+        // can it be null?
+        if ( !( ( rule.nullable !== undefined ) ? rule.nullable : false ) && v === null ) {
+          r.validates = false;
+          r.message = "Unexpected null in " + title;
+          return;
+        }
+        // is it of the right type?
+        r.message = "Type Mismatch; expected " + rule.type + " not " + ( typeof v ) + " in " + title;
+        switch ( rule.type ) {
+        case "number":
+          if ( v !== undefined ) {
+            if ( isNaN( parseFloat( v ) ) ) {
+              r.validates = false;
+              return;
+            }
+            if ( v != parseFloat( v ) ) {
+              r.validates = false;
+              return;
+            }
+          }
+          break;
+        case "integer":
+          if ( v !== undefined ) {
+            if ( isNaN( parseInt( v, 10 ) ) ) {
+              r.validates = false;
+              return;
+            }
+            if ( v != parseInt( v, 10 ) ) {
+              r.validates = false;
+              return;
+            }
+          }
+          break;
+        case "array":
+          if ( v !== undefined && !( v instanceof Array ) ) {
+            r.validates = false;
+            return;
+          }
+          break;
+        case "date":
+          if ( v instanceof Object ) {
+            if ( !( v instanceof Date ) ) {
+              r.validates = false;
+              return;
+            } else if ( v instanceof Date && isNaN( v.getTime() ) ) {
+              r.validates = false;
+              r.message = "Invalid date in " + title;
+              return;
+            }
+          } else if ( typeof v === "string" ) {
+            if ( isNaN( ( new Date( v ) ).getTime() ) ) {
+              r.validates = false;
+              r.message = "Invalid date in " + title;
+              return;
+            }
+          } else if ( !( v instanceof "object" ) && v !== undefined ) {
+            r.validates = false;
+            return;
+          }
+          break;
+        case "object":
+          if ( !( v instanceof Object ) && v !== undefined ) {
+            r.validates = false;
+            return;
+          }
+          break;
+        case "*":
+          break;
+        default:
+          if ( !( typeof v === rule.type || v === undefined ) ) {
+            r.validates = false;
+            return;
+          }
+        }
+        r.message = "";
+        // if we're still here, types are good. Now check length, range, and enum
+        // check range
+        r.message = "Value out of range " + v + " in " + title;
+        if ( typeof rule.min === "number" && v < rule.min ) {
+          r.validates = false;
+          return;
+        }
+        if ( typeof rule.max === "number" && v > rule.max ) {
+          r.validates = false;
+          return;
+        }
+        r.message = "";
+        // check length
+        if ( ( typeof rule.minLength === "number" && v !== undefined && v.length !== undefined && v.length < rule.minLength ) ||
+          ( typeof rule.maxLength === "number" && v !== undefined && v.length !== undefined && v.length > rule.maxLength ) ) {
+          r.message = "" + title + " out of length range";
+          r.validates = false;
+          return;
+        }
+        // check enum
+        if ( rule.enum instanceof Object && v !== undefined ) {
+          if ( rule.enum.filter( function ( e ) {
+            if ( e.value !== undefined ) {
+              return e.value == v;
+            } else {
+              return e == v;
+            }
+          } ).length === 0 ) {
+            r.message = "" + title + " contains unexpected value " + v + " in " + title;
+            r.validates = false;
+            return;
+          }
+        }
+        // check pattern
+        if ( rule.pattern instanceof Object && v !== undefined ) {
+          if ( v.match( rule.pattern ) === null ) {
+            r.message = "" + title + " doesn't match pattern in " + title;
+            r.validates = false;
+            return;
+          }
+        }
+      }
+    } );
+    return r;
+  }
   var _y = {
     VERSION: "0.5.142",
+    valueForKeyPath: valueForKeyPath,
+    interpolate: interpolate,
+    merge: merge,
+    validate: validate,
     /**
      * Returns an element from the DOM with the specified
      * ID. Similar to (but not like) jQuery's $(), except
@@ -3597,6 +3911,24 @@ define( 'yasmf/util/object',[],function () {
         return self;
       };
       self.on = self.addListenerForNotification;
+      /**
+       * Registers a listener valid for one notification only. Immediately after
+       * @method once
+       * @param  {[type]} theNotification [description]
+       * @param  {[type]} theListener     [description]
+       * @param  {[type]} async           [description]
+       * @return {[type]}                 [description]
+       */
+      self.once = function once( theNotification, theListener, async ) {
+        self.addListenerForNotification( theNotification, function onceHandler( sender, notice, args ) {
+          try {
+            theListener.apply( self, [ self, theNotification, args ].concat( arguments ) );
+          } catch ( err ) {
+            console.log( "ONCE Handler had an error", err );
+          }
+          self.removeListenerForNotification( theNotification, onceHandler );
+        }, async );
+      };
       /**
        * Removes a listener from a notification. If a notification has not been
        * registered (via `registerNotification`), an error is logged on the console
